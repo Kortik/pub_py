d116337c4da7c807a1a3c0c589b8f1fed6b8b021
Малахов Александр Вячеславович <AVyMalakhov@sberbank.ru>
2021-06-27 22:48:25 +0300

    Added major_version

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class HelloRequestHandler(BaseHTTPRequestHandler):
             </head>
             <body>
                 <h1>Hello, World!</h1>
-                <p>... Here we are! New Version!!Туц туц туц WAKA WAKA FU FU FUUU</p>
+                <p>... Here we are! New Version!!Туц туц туц WAKA WAKA FUГ FU FUUU</p>
             </body>
             </html>
         ''')
