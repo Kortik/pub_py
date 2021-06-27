99b47d7c257ff59f3ff91f288013cd7b11e6378c
Малахов Александр Вячеславович <AVyMalakhov@sberbank.ru>
2021-06-27 18:32:15 +0300

    test

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class HelloRequestHandler(BaseHTTPRequestHandler):
             </head>
             <body>
                 <h1>Hello, World!</h1>
-                <p>... Here we are! New Version!!ma!kjmjkxja</p>
+                <p>... Here we are! New Version!!ma!kjmjkxdja</p>
             </body>
             </html>
         ''')
