d75ac2d46b1a0d3493fc7be731028b9870172f14
Малахов Александр Вячеславович <AVyMalakhov@sberbank.ru>
2021-07-04 14:21:24 +0300

    sd

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class HelloRequestHandler(BaseHTTPRequestHandler):
             </head>
             <body>
                 <h1>Hello, World!</h1>
-                <p>... Here we are! New Version!!Туц туц туц WAKA WAKAA FUUUA FUUUAA FAU!UUAU!!!!!</p>
+                <p>... Here we are! New Version!!Туц туц туц WAKA WAKAA FUUUA FUUUAA FAU!UUAU!!!!!!</p>
             </body>
             </html>
         ''')
